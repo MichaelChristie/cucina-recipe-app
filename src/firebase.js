bc3702afa6d1213200be42c69e55f0ea7954d3c0
Michael Christie <christie.michael@gmail.com>
2024-12-01 17:49:49 +1300

    remapped the database to the cucine-recipe-app

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,25 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCinNok0vp-2IgFyOPFUmRNhi3WCxQL9wE",
-  authDomain: "my-recipe-app-6325e.firebaseapp.com",
-  projectId: "my-recipe-app-6325e",
-  storageBucket: "my-recipe-app-6325e.firebasestorage.app",
-  messagingSenderId: "873391643353",
-  appId: "1:873391643353:web:b6f17e9c70770a9cbffcc8",
-  measurementId: "G-39H9VPYYJF"
+  apiKey: "AIzaSyDyyjzMdzHZWTd8FKGp7MTLXpVPpx-dMGA",
+  authDomain: "cucina-recipe-app.firebaseapp.com",
+  projectId: "cucina-recipe-app",
+  storageBucket: "cucina-recipe-app.firebasestorage.app",
+  messagingSenderId: "571686975523",
+  appId: "1:571686975523:web:b708392d5a100969765996"
 };
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCinNok0vp-2IgFyOPFUmRNhi3WCxQL9wE",
+//   authDomain: "my-recipe-app-6325e.firebaseapp.com",
+//   projectId: "my-recipe-app-6325e",
+//   storageBucket: "my-recipe-app-6325e.firebasestorage.app",
+//   messagingSenderId: "873391643353",
+//   appId: "1:873391643353:web:b6f17e9c70770a9cbffcc8",
+//   measurementId: "G-39H9VPYYJF"
+// };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
