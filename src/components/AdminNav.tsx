6a4e15ee38872907e343acf1cb8ee405f2463065
Michael Christie <christie.michael@gmail.com>
2025-01-05 21:00:41 +1300

    minor error fix - stable

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -1,5 +1,15 @@
-{
-  name: 'Utilities',
-  href: '/admin/utils',
-  icon: CogIcon
-} 
+import { CogIcon } from '@heroicons/react/24/outline';
+
+interface NavigationItem {
+  name: string;
+  href: string;
+  icon: React.ComponentType;
+}
+
+const navigationItems: NavigationItem[] = [
+  {
+    name: 'Utilities',
+    href: '/admin/utils',
+    icon: CogIcon
+  }
+]; 
